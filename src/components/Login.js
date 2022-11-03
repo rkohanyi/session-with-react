75e992b6f1e20e5661bee62875f7bd57bb5809f2
Kohányi Róbert <kohanyi.robert@gmail.com>
2022-11-03 15:15:29 +0100

    Works for me

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,30 +1,39 @@
 import { useState } from "react"
 
-function Login() {
+function Login({onLogin}) {
     const [username, setUsername] = useState('joe')
     const [password, setPassword] = useState('joe')
 
     async function handleLogin() {
-        await fetch('/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                username,
-                password,
+        try {
+            const response = await fetch('/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    username,
+                    password,
+                })
             })
-        })
-        alert('Successful login!')
+            if (response.ok) {
+                onLogin()
+            } else {
+                onLogin(response.statusText)
+            }
+        } catch (error) {
+            onLogin(error)
+        }
     }
 
-    return <>
+    return <div>
+        <h1>Login</h1>
         Username: <input onChange={(e) => setUsername(e.target.value)} value={username} />
         <br />
         Password: <input onChange={(e) => setPassword(e.target.value)} value={password} />
         <br />
         <button onClick={handleLogin}>Login</button>
-    </>
+    </div>
 }
 
 export default Login
